b3fed0381ed3e94c53c6ed01e10dc257bae5520a
qunshiyan1 <2420980652@qq.com>
2025-09-26 19:15:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# 在此处编写代码
+year = input()
+if year.isdigit():
+    year = int(year)
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        print("是闰年")
+    else:
+        print("不是闰年")
+else:
+    print("输入错误")
